63d94e8894fd0a686f0272ad6b5bc281c7f9535e
nugraha-abd <nugrahaabdurahman@gmail.com>
2022-06-29 13:01:39 +0700

    Create a function to convert boolean to string based on its value

---
+++ 8-kyu/convert-boolean-to-string-yes-or-no.js
@@ -0,0 +1,13 @@
+/*
+
+Question 
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+*/
+
+// Answer
+
+function boolToWord(bool) {
+  return bool ? 'Yes' : 'No'
+}
