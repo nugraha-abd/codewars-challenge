684623f19fe84380eaf7637820454f68ebeb4d21
nugraha-abd <nugrahaabdurahman@gmail.com>
2022-06-29 12:46:24 +0700

    Create a function to reverse the strings

---
+++ 8-kyu/reversed-strings.js
@@ -0,0 +1,16 @@
+/*
+
+Question
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+*/
+
+// Answer
+
+function solution(str) {
+  return str.split('').reverse().join('')
+}
