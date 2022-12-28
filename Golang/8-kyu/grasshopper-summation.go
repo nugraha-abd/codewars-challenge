92627198a219935ba99d489d5ef34acee5d62a8c
nugraha-abd <nugrahaabdurahman@gmail.com>
2022-12-28 21:49:03 +0700

    Create a function to sum every number from 1 to num

--- Golang/8-kyu/grasshopper-summation.go
+++ Golang/8-kyu/grasshopper-summation.go
@@ -1,13 +1,11 @@
-package kata
+package Golang
 
-import "fmt"
 func Summation(n int) int {
-  var sum int
-  
-  for i := 0; i <= n; i++ {
-    sum += i
-    fmt.Println(i)
-  }
-  
-  return sum
+	var sum int
+
+	for i := 0; i <= n; i++ {
+		sum += i
+	}
+
+	return sum
 }
