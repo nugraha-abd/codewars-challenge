6a019e20abef40d660253e121cf235348f8f4952
nugraha-abd <nugrahaabdurahman@gmail.com>
2022-12-30 20:58:27 +0700

    Remove the comparison operator on checking whether the letter is lowercase

--- Golang/8-kyu/alternating-case.go
+++ Golang/8-kyu/alternating-case.go
@@ -8,7 +8,7 @@ func ToAlternatingCase(str string) string {
   var alternateStr string
   
   for _, char := range str {
-    if unicode.IsLower(char) == true {
+    if unicode.IsLower(char) {
       alternateStr += string(unicode.ToUpper(char))
     }else {
       alternateStr += string(unicode.ToLower(char))
