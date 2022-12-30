c0785f7b5354522b5a81e750241d8756e9a8ae93
nugraha-abd <nugrahaabdurahman@gmail.com>
2022-12-30 20:56:46 +0700

    Create a function to alternate every letter case from the given string

---
+++ Golang/8-kyu/alternating-case.go
@@ -0,0 +1,19 @@
+package Golang
+
+import (
+	"unicode"
+)
+
+func ToAlternatingCase(str string) string {
+  var alternateStr string
+  
+  for _, char := range str {
+    if unicode.IsLower(char) == true {
+      alternateStr += string(unicode.ToUpper(char))
+    }else {
+      alternateStr += string(unicode.ToLower(char))
+    }
+  }
+  
+  return alternateStr
+}
