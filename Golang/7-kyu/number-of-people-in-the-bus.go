f16ef74957b76c7663d40e90d49c4523da63f80f
nugraha-abd <nugrahaabdurahman@gmail.com>
2023-01-06 19:18:43 +0700

    Create a function to calculate how many people still in the bus after the last bus stop

--- Golang/7-kyu/number-of-people-in-the-bus.go
+++ Golang/7-kyu/number-of-people-in-the-bus.go
@@ -1,12 +1,12 @@
 package Golang
 
 func Number(busStops [][2]int) int {
-  var in, out int
-  
-  for _, v := range busStops {
-    in += v[0]
-    out += v[1]
-  }
-  
-  return in - out
+	var in, out int
+
+	for _, v := range busStops {
+		in += v[0]
+		out += v[1]
+	}
+
+	return in - out
 }
