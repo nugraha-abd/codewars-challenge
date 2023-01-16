b2c2d919da7938ec35ef9ee8bcfee6d79112176f
nugraha-abd <nugrahaabdurahman@gmail.com>
2023-01-16 20:20:44 +0700

    Create a function that returns true if the given number is a negative zero and vice versa

---
+++ Golang/7-kyu/is-it-negative-zero.go
@@ -0,0 +1,13 @@
+package kata
+
+import (
+	"fmt"
+	"math"
+)
+
+func IsNegativeZero(n float64) bool {
+  if fmt.Sprint(n) == fmt.Sprint(math.Copysign(0, -1)) {
+    return true
+  }
+  return false
+}
