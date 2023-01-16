d2f3a93b73e6659089cc1f3b1ce26091e2b7144d
nugraha-abd <nugrahaabdurahman@gmail.com>
2023-01-16 20:46:26 +0700

    Refactor: remove conditional statement

--- Golang/7-kyu/is-it-negative-zero.go
+++ Golang/7-kyu/is-it-negative-zero.go
@@ -6,8 +6,5 @@ import (
 )
 
 func IsNegativeZero(n float64) bool {
-  if fmt.Sprint(n) == fmt.Sprint(math.Copysign(0, -1)) {
-    return true
-  }
-  return false
+  return fmt.Sprint(n) == fmt.Sprint(math.Copysign(0, -1))
 }
